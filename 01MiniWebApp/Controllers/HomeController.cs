6df4638a5baca5aca58a1ea0b23ee5eb49493a3c
Suleyman SH. Suleymanli <SuleymanSHs@CODE.LOCAL>
2019-02-11 21:22:54 +0400

    first

---
+++ 01MiniWebApp/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiniWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniWebApp.Controllers
+{
+    public class HomeController:Controller
+    {
+
+        public UserDbContext _userDbContext { get; set; }
+
+        public HomeController(UserDbContext userDbContext)
+        {
+            this._userDbContext = userDbContext;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(_userDbContext.Users.Include(m => m.Name).ToListAsync());
+        }
+
+    
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                await _userDbContext.Users.AddAsync(user);
+                await _userDbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(user));
+            }
+            
+
+            return View();
+        }
+
+
+
+    }
+}
---
+++ 01MiniWebApp/Models/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniWebApp.Models
+{
+    public class User
+    {
+         public int Id { get; set; }
+        
+         [Required]
+         [MinLength(3)]
+         public string Name { get; set; }
+
+
+
+         [Required]
+         [MinLength(6)]
+         public string Surname { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Range(0,1)]
+        public GenderStatus gender { get; set; }
+
+         
+
+
+    
+
+}
+}
